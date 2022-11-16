1b38e074f37b259be7117435c1d8f4ad4d01b4b0
rowenahosie <rjhosie@gmail.com>
2022-11-16 18:36:34 +0000

    Works with 1 character
    Works with 1 character

--- RomanNumberalConverter.py
+++ RomanNumberalConverter.py
@@ -1,1 +1,11 @@
-print("hello world")
+print("Enter a Roman Numeral to convert to decimal: ")
+numeral = raw_input()
+
+RomanDecimalMap = {
+	"I" : 1,
+	"V" : 5,
+	"X" : 10
+}
+
+print(RomanDecimalMap[numeral])
+
