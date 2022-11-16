0c92e2c198a329e97016feffcace5e0aaa2ea6b4
rowenahosie <rjhosie@gmail.com>
2022-11-16 20:46:30 +0000

    renamed notebook

--- RomanNumberalConverter.py
+++ RomanNumberalConverter.py
@@ -5,7 +5,7 @@ RomanList = ["M", "CM", "D", "CD", "C", "XC","L", "XL", "X","IX", "V", "IV", "I"
 DecimalList = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
 temp = int(num)
-output_string = ""
+output_string = "" 
 
 for i, decimal in enumerate(DecimalList):
 	while(temp>=decimal):
