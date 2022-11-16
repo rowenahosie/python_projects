ffe73c8ef6e1656ab74aa2bbf32075b02e73aede
rowenahosie <rjhosie@gmail.com>
2022-11-16 20:17:02 +0000

    Update RomanNumberalConverter.py
    Works up to 1000

--- RomanNumberalConverter.py
+++ RomanNumberalConverter.py
@@ -1,15 +1,15 @@
 print("Enter a decimal to convert to Roman numeral: ")
 num = raw_input()
 
-RomanList = ["X","IX", "V", "IV", "I"]
-DecimalList = [10, 9, 5, 4, 1]
+RomanList = ["M", "CM", "D", "CD", "C", "XC","L", "XL", "X","IX", "V", "IV", "I"]
+DecimalList = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
 temp = int(num)
 output_string = ""
 
 for i, decimal in enumerate(DecimalList):
 	while(temp>=decimal):
-		temp -= decimal 
+		temp -= decimal
 		output_string += RomanList[i]
 
 print(output_string)
